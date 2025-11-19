c33321bc45c2be6cb215b10d33dc53b1481433ec
quothraven1122 <102286920+quothraven1122@users.noreply.github.com>
2025-11-19 16:36:09 +0900

    feat: set up server

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import express from "express";
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on http://localhost:${PORT}`);
+});
