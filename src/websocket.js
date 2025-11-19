dae0c48087aac190efc107b52dc40e8cddb10b02
quothraven1122 <102286920+quothraven1122@users.noreply.github.com>
2025-11-19 22:23:31 +0900

    feat: create match making logic

--- src/websocket.js
+++ src/websocket.js
@@ -1,20 +1,68 @@
 import { WebSocketServer } from "ws";
+import { randomUUID } from "crypto";
+
+let waitingPlayer = null;
+const rooms = {};
 
 export function initWebSocket(server) {
   const wss = new WebSocketServer({ server });
-  let players = [];
-  let gameState = { p1: null, p2: null };
 
   wss.on("connection", (ws, req) => {
-    players.push(ws);
-    console.log(players.length);
+    console.log("New Player Connected");
+    // 1) make player wait if no one is waiting
+    if (!waitingPlayer) {
+      waitingPlayer = ws;
+      ws.send(JSON.stringify({ type: "waiting" }));
+      return;
+    }
 
-    ws.on("message", (msg) => {
-      console.log("Received:", msg.toString());
+    // 2) if two players are collected - make a room
+    const roomId = randomUUID();
+    rooms[roomId] = {
+      players: [waitingPlayer, ws],
+      board: createInitialBoard(),
+    };
+    waitingPlayer.roomId = roomId;
+    waitingPlayer.playerId = "p1";
+    ws.roomId = roomId;
+    ws.playerId = "p2";
+
+    //notify both players of the match
+    rooms[roomId].players.forEach((p) => {
+      p.send(
+        JSON.stringify({
+          type: "matched",
+          roomId,
+          youAre: p.playerId,
+          board: rooms[roomId].board,
+        })
+      );
     });
+    waitingPlayer = null;
 
     ws.on("close", () => {
-      console.log("❌ Client disconnected");
+      const roomId = ws.roomId;
+      if (!roomId) return;
+
+      const room = rooms[roomId];
+      if (!room) return;
+
+      //find remaining player
+      const otherPlayer = room.players.find((p) => p !== ws);
+
+      //delete room
+      delete rooms[roomId];
+
+      //put remaining player back in matchmaking
+      if (otherPlayer && otherPlayer.readyState === otherPlayer.OPEN) {
+        waitingPlayer = otherPlayer;
+        otherPlayer.roomId = null;
+        otherPlayer.playerId = null;
+
+        otherPlayer.send(JSON.stringify({ type: "waiting" }));
+      }
+
+      console.log(`Room ${roomId} destroyed because a player disconnected`);
     });
   });
 }
